d86af2d04a3bcdbbc5e949a3081cc156c8fec900
Kasper Sonne <kaspersonne27@gmail.com>
2020-05-23 13:13:12 +0200

    Redux Setup & Alerts section completed

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,12 @@
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => (dispath) => {
-  const id = uuid.v4();
+export const setAlert = (msg, alertType, timeout = 5000) => (dispath) => {
+  const id = uuidv4();
   dispath({
-      type: SET_ALERT:
-      payload: { msg, alertType, id }
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
   });
+
+  setTimeout(() => dispath({ type: REMOVE_ALERT, payload: id }), timeout);
 };
